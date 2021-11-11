e6146acfe7d5fa52821aea8044930823ea6cdede
HobbitForHire <mmayes1997@gmail.com>
2021-11-11 21:55:50 +0000

    Initial commit

---
+++ src/main/java/lotc/hobbit/hoesPun/MainHoes.java
@@ -0,0 +1,17 @@
+package lotc.hobbit.hoesPun;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MainHoes extends javaPlugin {
+
+    //Overides for Enabled and Disabled Plugin
+    //Something to check with TT. Not sure they're needed, but worth addint
+    @Override
+    public void onEnable(){}
+    @Override
+    public void onDisable(){}
+
+
+
+
+}
